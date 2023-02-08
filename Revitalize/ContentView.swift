b44bc3ee6f613e3bc6c54f6e295f607a01a848e4
Andrea Reed <34424966+VoitekGraphics@users.noreply.github.com>
2023-02-08 00:27:25 -0500

    Dashboard Components

--- Revitalize/ContentView.swift
+++ Revitalize/ContentView.swift
@@ -18,27 +18,50 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            ScrollView{
+                ZStack{
+                    VStack{
+                        HStack{
+                       Image("Revitalize")
+                                
+                            VStack{
+                                Image("Login")
+                                Image("SignUp")
+                            }
+                        }
+                        HStack{
+                            Image("CurrentProject")
+                        }
+                        HStack{
+                            Image("HomeOwnersGuide")
+                        }
+                        HStack{
+                            Image("MenuBar")
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+//            List {
+//                ForEach(items) { item in
+//                    NavigationLink {
+//                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+//                    } label: {
+//                        Text(item.timestamp!, formatter: itemFormatter)
+//                    }
+//                }
+//                .onDelete(perform: deleteItems)
+//            }
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    EditButton()
+//                }
+//                ToolbarItem {
+//                    Button(action: addItem) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                }
+//            }
+//            Text("Select an item")
         }
     }
 
