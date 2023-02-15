d3a227b75c382423a8d2d33b25c15d2258150760
Andrea Reed <34424966+VoitekGraphics@users.noreply.github.com>
2023-02-14 19:29:05 -0500

    Working on Contractor Profile

--- Revitalize/ContractorProfile.swift
+++ Revitalize/ContractorProfile.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContractorProfile: View {
     @State var companyName: String = ""
+    @State var email: String = ""
     @State var licenseNumber: String = ""
     @State var companyAddress: String = ""
     @State var city: String = ""
@@ -29,7 +30,18 @@ struct ContractorProfile: View {
                 Image("Contractor Profile")
                 Form{
                     TextField("Company Name", text: $companyName)
+                    TextField("Email",text:  $email)
                     TextField("License Number",text:  $licenseNumber)
+                    TextField("Company Address",text:  $companyAddress)
+                    TextField("City",text:  $city)
+                    TextField("State",text:  $state)
+                    TextField("Zip Code",text:  $zipCode)
+                }
+                Form{
+                    TextField("First Name",text:  $firstName)
+                    TextField("Last Name",text:  $lastName)
+                    TextField("Title",text:  $title)
+                    TextField("Phone Number",text:  $phoneNumber)
                 }
             }
         }
