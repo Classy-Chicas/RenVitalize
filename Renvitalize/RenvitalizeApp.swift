83b62976f9b177aee7d87635ddf221c87b3fbb78
Andrea Reed <34424966+VoitekGraphics@users.noreply.github.com>
2023-03-12 12:55:44 -0400

    Initial Commit

---
+++ Renvitalize/RenvitalizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenvitalizeApp.swift
+//  Renvitalize
+//
+//  Created by Andrea Reed on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RenvitalizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
